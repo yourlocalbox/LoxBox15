16099ae45de75f3b1175abe69082fd51b916e463
Ivo Tamboer <ivo@libbit.eu>
2015-09-22 14:50:39 +0200

    nothing happened (or should have)

--- database.sql
+++ database.sql
@@ -1,6 +1,7 @@
-create table shareitem (icon char(255), path char(255), has_keys boolean, is_share boolean, is_shared boolean, modified_at datetime, title char(255), is_dir boolean);
+CREATE TABLE shareitem (icon char(255), path char(255), has_keys boolean, is_share boolean, is_shared boolean, modified_at datetime, title char(255), is_dir boolean);
 CREATE TABLE shares (id integer primary key autoincrement, user char(255), path char(255));
 CREATE TABLE invitations (id INTEGER PRIMARY KEY AUTOINCREMENT, sender char(255), receiver char(255), share_id int, state char(255), FOREIGN KEY (share_id) REFERENCES share(id));
+CREATE TABLE users (name char(255), public_key char(255), private_key char(255));
 
 
 
@@ -11,3 +12,4 @@ insert into shareitem VALUES('', 'path', 0, 0, 0, DATETIME(), 'path', 0);
 insert into shareitem VALUES('', 'path2', 0, 0, 0, DATETIME(), 'path2', 0);
 
 insert into shares (user, path) VALUES ('user', 'path');
+insert into users VALUES('user', 'public_key_component', 'private_key_component');
