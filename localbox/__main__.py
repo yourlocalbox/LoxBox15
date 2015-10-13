66c1bf4ba8775030b0bda42b533556f09d92ff83
Ivo Tamboer <ivo@libbit.eu>
2015-10-13 12:09:02 +0200

    added SIGINT signal handler

--- localbox/__main__.py
+++ localbox/__main__.py
@@ -1,16 +1,26 @@
 """
 Start the LocalBox server
 """
-from localbox import main
 from logging import DEBUG
 from logging import StreamHandler, FileHandler
 from logging import Formatter
+from logging import getLogger
+from signal import SIGINT, signal
+from sys import exit as sysexit
 
+from .__init__ import main
 from .config import prepare_logger
 from .config import ConfigSingleton
 
 
+def sig_handler(signum, frame):
+    if signum == SIGINT:
+        getLogger('api').info('SIGINT received, shutting down')
+        #TODO: Graceful shutdown that lets people finish their things
+        sysexit(1)
+
 def run():
+    signal(SIGINT, sig_handler)
     config = ConfigSingleton()
     formatter = Formatter('%(asctime)s %(ip)s %(user)s: %(message)s')
     logfile = config.get('logging', 'logfile')
