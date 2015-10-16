9422a6254e27393a9354f74a0fbfb8dda9119c3a
Ivo Tamboer <ivo@libbit.eu>
2015-10-16 12:08:34 +0200

    change rpm building to use supplied specfile

--- tools/gendist.sh
+++ tools/gendist.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 python setup.py bdist_dumb
 python setup.py bdist_egg
-python setup.py bdist_rpm
 python setup.py bdist_wininst
+#python setup.py bdist_rpm
+
+cp dist/localbox-1.8.0.tar.gz ~/rpmbuild/SOURCES
+cp conf/localbox.inuit.d ~/rpmbuild/SOURCES
+rpmbuild -ba conf/localbox.spec && cp ~/rpmbuild/RPMS/*/localbox*.rpm .
+
