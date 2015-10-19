a81710a4d26c2cae4f0be3a68930f0531d86e524
Ivo Tamboer <ivo@libbit.eu>
2015-10-19 10:44:10 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ setup(
     name = "localbox",
     version = "1.8.0",
     packages = find_packages(),
-    author = "EZ",
+    author = "Letshare Holding B.V.",
     author_email = "ivo@libbit.eu",
     description = "",
-    license =  "EUPL",
+    license =  "all rights reserved",
     url = "http://box.yourlocalbox.org",
 )
