1ee0b7481a4758bbde150acc1b8c19cb3b159bca
Ivo Tamboer <ivo@libbit.eu>
2016-04-12 12:46:04 +0200

    updated gendist to generate rpms

--- tools/gendist.sh
+++ tools/gendist.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 python setup.py bdist_dumb
 python setup.py bdist_egg
 python setup.py bdist_wininst
 #python setup.py bdist_rpm
 
-cp dist/localbox-1.8.0.tar.gz ~/rpmbuild/SOURCES
-cp conf/localbox.inuit.d ~/rpmbuild/SOURCES
-rpmbuild -ba conf/localbox.spec && cp ~/rpmbuild/RPMS/*/localbox*.rpm .
+archivename=`cat conf/localbox.spec | grep "Source0:" | sed "s/Source0:[ \t]\+//"`
+git archive --format=tar.gz --prefix=localbox-1.8.0/ -o "dist/$archivename" HEAD
 
+cp dist/localbox-1.8.0.tar.gz ~/rpmbuild/SOURCES
+cp conf/localbox.init.d ~/rpmbuild/SOURCES
+rpmbuild -ba conf/localbox.spec && cp ~/rpmbuild/RPMS/*/localbox*.rpm dist
