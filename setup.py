ffdcbea8b2889f5a92bc7a383bdc20cd4016d850
wilson-santos-pf <wilson.santos@penguinformula.com>
2016-11-17 11:48:14 +0000

    setup author

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name = "localbox",
-    version = "1.8.0",
+    version = "1.6.0",
     packages = find_packages(),
-    author = "Letshare Holding B.V.",
-    author_email = "ivo@libbit.eu",
-    description = "",
-    license =  "all rights reserved",
-    url = "http://box.yourlocalbox.org",
+    description='Backend implementation of LocalBox',
+    author="De Staat der Nederlanden",
+    author_email="info@yourlocalbox.org",
+    url="https://yourlocalbox.org",
+    license="EUPL"
 )
