ce4a3f7ae1fa98ced439adcc2c3775efccf6896d
wilson-santos-pf <wilson.santos@penguinformula.com>
2017-01-06 11:43:02 +0000

    defaults documentation

--- localbox/defaults.py
+++ localbox/defaults.py
@@ -1,6 +1,9 @@
 import socket
 
+#: port where the Loauth server is listening to requests
 LOAUTH_PORT = 5000
+#: Loauth URL for verifying if the bearer authorization token is expired
 VERIFY_URL = 'http://%s:%d/verify' % (socket.gethostname(), LOAUTH_PORT)
+#: Loauth URL for validating authorization request
 REDIRECT_URL = 'http://%s:%s/loauth/' % (socket.gethostname(), LOAUTH_PORT)
 DIRECT_BACK_URL = 'http://%s' % socket.gethostname()
